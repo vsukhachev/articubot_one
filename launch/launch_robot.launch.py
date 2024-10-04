3380fbe0e8bb65a026946cdac982f8a62b34d686
Vlad <vsukhachev@gmail.com>
2024-10-04 18:40:14 -0400

    Refactor launch files to remove unused twist_mux node

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -35,33 +35,27 @@ def generate_launch_description():
     # )
 
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
-
-    
-
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    #twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    #twist_mux = Node(
+    #        package="twist_mux",
+    #        executable="twist_mux",
+    #        parameters=[twist_mux_params],
+    #        remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    #    )
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[controller_params_file]
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -74,7 +68,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
@@ -108,7 +102,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         # joystick,
-        twist_mux,
+        #twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
